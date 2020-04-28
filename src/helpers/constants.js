79fb836f7f387f744944ccd4d6df26ba2b7bc467
Janamajaya Mall <janmajaya.choithram@gmail.com>
2020-04-28 18:14:34 +0530

    ios live

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -79,13 +79,13 @@ export const constants = {
     },
     terms_and_conditions:
         Platform.OS==="ios" ?
-        `
+`
 Tornado App End User License Agreement 
 
 This End User License Agreement (“Agreement”) is between you and Tornado and governs use of this app made available through the Apple App Store. By installing the Tornado App, you agree to be bound by this Agreement and \
 understand that there is no tolerance for objectionable content. If you do not agree with the terms and conditions of this Agreement, you are not entitled to use the Tornado App. 
 
-In order to ensure Tornado provides the best experience possible for everyone, we strongly enforce a no tolerance policy for objectionable content.If you see inappropriate content, please use the “Report as offensive” feature found under each post. 
+In order to ensure Tornado provides the best experience possible for everyone, we strongly enforce a no tolerance policy for objectionable content.If you see inappropriate content, please use the “Report” feature found under each post. 
 
 1. Parties
 This Agreement is between you and Tornado only, and not Apple, Inc. (“Apple”). Notwithstanding the foregoing, you acknowledge that Apple and its subsidiaries are third party beneficiaries of this Agreement and Apple has the right to enforce this Agreement against you. Tornado, not Apple, is solely responsible for the Tornado App and its content. 
@@ -114,6 +114,26 @@ Tornado, not Apple, is responsible for addressing any claims by you relating to 
 
 9. Third Party Intellectual Property Claims
 Tornado shall not be obligated to indemnify or defend you with respect to any third party claim arising out or relating to the Tornado App. To the extent Tornado is required to provide indemnification by applicable law, Tornado, not Apple, shall be solely responsible for the investigation, defense, settlement and discharge of any claim that the Tornado App or your use of it infringes any third party intellectual property right. 
-        `:
-        "d"
+`:
+`
+Tornado App End User License Agreement 
+
+By installing the Tornado App, you agree to be bound by this Agreement and \
+understand that there is no tolerance for objectionable content. If you do not agree with the terms and conditions of this Agreement, you are not entitled to use the Tornado App. 
+
+In order to ensure Tornado provides the best experience possible for everyone, we strongly enforce a no tolerance policy for objectionable content.If you see inappropriate content, please use the “Report” feature found under each post. 
+
+1. Privacy
+Tornado may collect and use information about your usage of the Tornado App, including certain types of information from and about your device. Tornado may use this information, as long as it is in a form that does not personally identify you, to measure the use and performance of the Tornado App. 
+
+2. Limited License
+Tornado grants you a limited, non-exclusive, non-transferable, revocable license to use the Tornado App for your personal, non-commercial purposes. 
+
+3. Age Restrictions 
+By using the Tornado App, you represent and warrant that
+(a) you are 17 years of age or older and you agree to be bound by this Agreement; (b) if you are under 17 years of age, you have obtained verifiable consent from a parent or legal guardian; and (c) your use of the Tornado App does not violate any applicable law or regulation. Your access to the Tornado App may be terminated without warning if Tornado believes, in its sole discretion, that you are under the age of 17 years and have not obtained verifiable consent from a parent or legal guardian. If you are a parent or legal guardian and you provide your consent to your child’s use of the Tornado App, you agree to be bound by this Agreement in respect to your child’s use of the Tornado App. 
+
+4. Objectionable Content Policy
+Content may not be submitted to Tornado, who will moderate all content and ultimately decide whether or not to post a submission to the extent such content includes, is in conjunction with, or alongside any, Objectionable Content. Objectionable Content includes, but is not limited to: (i) sexually explicit materials; (ii) obscene, defamatory, libelous, slanderous, violent and/or unlawful content or profanity; (iii) content that infringes upon the rights of any third party, including copyright, trademark, privacy, publicity or other personal or proprietary right, or that is deceptive or fraudulent; (iv) content that promotes the use or sale of illegal or regulated substances, tobacco products, ammunition and/or firearms; and (v) gambling, including without limitation, any online casino, sports books, bingo or poker.
+`
 }
